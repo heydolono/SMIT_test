2d1a85fa5059b4f6716af3384ae32476c21091ed
heydolono <makskol43@gmail.com>
2024-11-18 17:14:38 +0300

    deploy

--- app/core/db.py
+++ app/core/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Date, Float, Integer, String
+from sqlalchemy import Column, Integer
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, declared_attr, sessionmaker
 
